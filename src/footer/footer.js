44e7a34f01fa301e7de7d7492fb7a3f410882e27
Elizabeth Means <e.means@kunzleigh.com>
2022-03-06 14:06:57 -0500

    Social media footer links

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './footer.scss';
 
 export default function Footer() {
@@ -8,9 +9,33 @@ export default function Footer() {
             </div>
             
             <div className='footer-content'>
-                <div className='social-media-links'>
+                <div>
                     <h2>Let's connect!</h2>
-                    <div>
+                    <div className='social-media-links'>
+                        <a
+                            href="https://www.linkedin.com/in/elizabeth-a-means/"
+                            target='_blank'
+                            rel="noopener"
+                            aria-label='LinkedIn'>
+                                <div className='linkedin'></div>
+                        </a>
+                        <a
+                            href="https://github.com/emeans"
+                            target='_blank'
+                            rel="noopener"
+                            aria-label='Github'>
+                                <div className='github'></div>
+                        </a>
+                        <a
+                            href="https://codepen.io/emeans"
+                            target='_blank'
+                            rel="noopener"
+                            aria-label='Codepen'>
+                                <div className='codepen'></div>
+                        </a>
+                        {/* Behance */}
+                        {/* Dribbble */}
+                        {/* Instagram */}
                     </div>
                 </div>
                 <div className='contact-form'>
