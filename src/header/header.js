ffc757a9c86666f8642ff2fcb1ed73c6ff7feff2
Elizabeth Means <e.means@kunzleigh.com>
2022-03-07 14:14:00 -0500

    Scaffold homepage (#11)
    * Update favicon for now
    
    * Breakout the pop out portrait
    
    * Add Adobe typekit fonts
    
    * Add router & routes
    
    * Make github happy
    
    * WIP - scaffolding
    
    * Social media footer links
    
    * Footer scaffold & tweaks
    
    * More social & sticky footer
    
    * Under construction
    
    * Update dependencies
    
    * Case sensitive mv -f
    
    * No unused vars
    
    * Better height calc on header
    
    * Setup form for netlify
    
    * Doesn't like commented out code. Okay.
    
    * Error page & turn on under construction
    
    * Remove debugger & cleanup

---
+++ src/header/header.js
@@ -0,0 +1,19 @@
+import PopOutPortrait from '../portrait/pop-out-portrait';
+import './header.scss';
+
+export default function Header() {
+    return(
+        <header>
+            <section className='hero'>
+                <div className='introduction'>
+                    <div className='intro-text'>
+                        <h1>Hey there, I'm Elizabeth… </h1>
+                        <p>Pommy ipsum smeg head whizz morris dancers come hither, bugger codswallop gob. Taking the mick middle class bog roll bow ties are cool posh nosh off t'shop, stew and dumps taking the mick know your onions pulled a right corker 'tis, anorak mince pies tosser warts and all knackered, complete mare stupendous proper on the beat.</p>
+                    </div>
+                    <PopOutPortrait></PopOutPortrait>
+                </div>
+                <div className='hero-rip'></div>
+            </section>
+       </header>
+    );
+}
---
+++ src/routes/under-construction.js
@@ -0,0 +1,34 @@
+import './under-construction.scss';
+
+export default function UnderConstruction() {
+
+    /* Set width of all animated text to match container */
+    let parent = document.querySelectorAll('.animate-text');
+    for(let i = 0; i < parent.length; i++) {
+        parent[i].style.width = parent[i].children[0].clientWidth + "px"; 
+    };
+
+    return (
+        <div className='under-construction'>
+            <div className="bg-text-container">
+                <div className="animate-text">
+                    <span>coming soon&nbsp;</span>
+                    <span>under construction&nbsp;</span>
+                </div>
+                <div className="animate-text left">
+                    <span>coming soon&nbsp;</span>
+                    <span>under construction&nbsp;</span>
+                </div>
+            </div>
+        
+            <div className="container">
+                <div className="col">
+                    <h1>Hi there…</h1>
+                    <p>As you can see, I don't have any of my work posted right now. I wish I had more to show you, but I want my work to be of the highest quality and will upload it ASAP. I appreciate you stopping by and I apologize for the inconvenience. Please check back soon!</p>
+                    <br/>
+                    <p className='signature'>Elizabeth Means</p>
+                </div>
+            </div>
+        </div>
+    );
+}
