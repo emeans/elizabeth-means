a3ff393193d59c3d87382e9084c44e23e1cb2340
Elizabeth Means <e.means@kunzleigh.com>
2022-03-07 13:49:44 -0500

    Setup form for netlify

--- src/footer/contact-form.js
+++ src/footer/contact-form.js
@@ -15,23 +15,19 @@ export default class ContactForm extends React.Component {
     }
 
     handleChange(event) {
-        // debugger;
         this.setState({
-            // Computed property names
-            // keys of the objects are computed dynamically
             [event.target.name] : event.target.value
-          })
+        });
     }
   
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.name + ' with email: ' + this.state.email + ' and message: ' + this.state.message);
-
-      event.preventDefault();
-    }
+    // handleSubmit(event) {
+    //   alert('A name was submitted: ' + this.state.name + ' with email: ' + this.state.email + ' and message: ' + this.state.message);
+    //   event.preventDefault();
+    // }
   
     render() {
       return (
-        <form name='contact' method='POST' data-netlify='true' onSubmit={this.handleSubmit.bind(this)}>
+        <form name='contact' method='POST' data-netlify='true'>
             <input aria-label='name' name='name' placeholder='Name' type='text' value={this.state.name} onChange={this.handleChange} />
             <input aria-label='email' name='email' placeholder='Email' type='text' value={this.state.email} onChange={this.handleChange} />
             <textarea aria-label='message' name='message' placeholder='Message' type='text' rows='5' value={this.state.message} onChange={this.handleChange} />
