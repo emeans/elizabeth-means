8ea9ee90bd161a18884d76a7562dd008b4a665a0
Elizabeth Means <e.means@kunzleigh.com>
2022-03-28 17:18:08 -0400

    Some lints

--- src/navigation/mobile-navigation.tsx
+++ src/navigation/mobile-navigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import { IMobileMenuState, INavigationMenuProps } from '../types'
 import MobileMenuButton from './mobile-menu-button'
 import MobileMenuContent from './mobile-menu-content'
