4bbc3027e840404f476f0a0a84a6a7851eba728d
Elizabeth Means <elizabeth.a.means@gmail.com>
2025-11-15 11:59:09 -0500

    Remove image references

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -7,12 +7,6 @@ export default function Hero() {
     <section id='home' className={styles.hero}>
       <div className='container'>
         <div className={styles.heroContent}>
-          {/* <div className={styles.heroImage}>
-            <img
-                src='/portrait.png'
-                alt='Portrait photo of Elizabeth Means, Product Designer & Engineer'
-            />
-          </div> */}
           <div className={styles.heroText}>
             <h1><span className={styles.highlight}>Hi, I'm Elizabeth&nbsp;Means</span></h1>
             <p className={styles.heroSubtitle}><span className={styles.highlight}>Product Designer & Engineer</span></p>
