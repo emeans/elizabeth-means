83b67cacb1a9a2d0655333ee5a4e35d9809878b4
Elizabeth Means <elizabeth.a.means@gmail.com>
2026-02-24 16:22:44 -0500

    Remove ambiguous hero image - so plain.

--- app/features/home/Hero/Hero.tsx
+++ app/features/home/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from '@components/Link'
 import styles from './Hero.module.css'
 
 export default function Hero() {
@@ -6,10 +7,21 @@ export default function Hero() {
       <div className='container'>
         <div className={styles.heroContent}>
           <div className={styles.heroText}>
-            <h1><span>Hi, I'm Elizabeth&nbsp;Means</span></h1>
-            <p className={styles.heroSubtitle}><span>I build the systems that help teams do their best work.</span></p>
+            <h1>Hi, I'm Elizabeth&nbsp;Means
+            </h1>
+            <p className={styles.heroSubtitle}>
+              I build the systems that help teams do their best work.
+            </p>
             <p className={styles.heroDescription}>
-              <span>Product strategist, design thinker, and engineer — bridging user needs, technical constraints, and human-centered culture.</span>
+                Product strategist, design thinker, and engineer — bridging user needs, technical
+                constraints, and human-centered culture.
+            </p>
+            <hr className='section-divider' />
+            <p>
+              <span className={styles.heroWork}>Building the Playbook While Running the Play:</span>
+              <span className={styles.heroWorkSecondary}>0-to-1 Product Management at KL&A</span>
+              <span><Link variant='inline' href='/work/wic-product-case-study'>Read the case study</Link></span>
+
             </p>
           </div>
         </div>
